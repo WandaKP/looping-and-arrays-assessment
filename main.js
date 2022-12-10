13b7c0cd477d1a62ec83b00eba7788f44fdc12a2
Wanda <wandaplaza1031@gmail.com>
2022-12-10 09:40:57 -0500

    Completed Loops

---
+++ main.js
@@ -0,0 +1,15 @@
+for(let i = 1000; i >= 0; i--) {
+    if(i % 2 === 0) {
+        console.log(i)
+    }
+}
+
+for(let i = 0; i <= 10000; i++) {
+    if(i === 2500) {
+        alert("A quarter of the way there!")
+    } else if (i === 5000) {
+        alert("Halfway there!")
+    } else if  (i === 10000) {
+        alert("The loop is done!")
+    }
+}
